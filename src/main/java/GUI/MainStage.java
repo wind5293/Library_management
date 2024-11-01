ccf8d6e8d49130a1102b095b2f10ff053d9ec800
wind5293 <mywind5293@gmail.com>
2024-11-01 22:05:56 +0700

    update main stage v0.2.1, pull lại vì Hùng muốn nhờ

--- src/main/java/GUI/MainStage.java
+++ src/main/java/GUI/MainStage.java
@@ -1,11 +1,23 @@
 package GUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 
 public class MainStage {
+    @FXML
+    private TextField searchText;
+    @FXML
+    private Button searchButton;
+
+    public void searchButtonClicked(ActionEvent event) {
+        String searchContent = searchText.getText();
+        System.out.println(searchContent);
+    }
 
 
 }
