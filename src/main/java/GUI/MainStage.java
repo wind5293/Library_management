448633cb76442e30e84d2b4f2fa907abb2a84148
wind5293 <mywind5293@gmail.com>
2024-11-05 20:57:14 +0700

    Change on MainStage.java

--- src/main/java/GUI/MainStage.java
+++ src/main/java/GUI/MainStage.java
@@ -14,14 +14,15 @@ public class MainStage {
     @FXML
     private Button searchButton;
 
-    public String searchButtonClicked(ActionEvent event) {
-        String searchContent = searchText.getText();
-<<<<<<< HEAD
-
-=======
-        return searchContent;
->>>>>>> 5aff11eb5d97483f30ad12ec0bc82d8e501fafe8
-    }
+//    @FXML
+//    private TextField searchText;
+//    @FXML
+//    private Button searchButton;
+//
+//    public String searchButtonClicked(ActionEvent event) {
+//        String searchContent = searchText.getText();
+//        return searchContent;
+//    }
 
 
 }
