4eeadfacbc104ec782da6d88be623ce51e05408a
wind5293 <mywind5293@gmail.com>
2024-11-25 18:42:58 +0700

    RegisterStage v0.3 with bug

--- src/main/java/GUI/RegisterStage.java
+++ src/main/java/GUI/RegisterStage.java
@@ -1,19 +1,14 @@
 package GUI;
 
-import DataBaseSQL.BookDataBase;
 import DataBaseSQL.UserDataBase;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,22 +17,23 @@ import java.sql.SQLException;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class RegisterStage {
+public class RegisterStage implements Initializable {
     @FXML
-    private Button button_cfsignup_reg;
-
+    private Button SignUpButton;
     @FXML
-    private Button button_login_reg;
-
+    private Button LoginButton;
     @FXML
-    private TextField tf_username;
-
+    private TextField UserNameTF;
     @FXML
-    private TextField tf_password;
-
+    private TextField EmailTF;
     @FXML
-    private TextField tf_cfpassword;
-
+    private TextField AddressTF;
+    @FXML
+    private TextField PasswordTF;
+    @FXML
+    private TextField CFPasswordTF;
+    @FXML
+    private ComboBox<Integer> AgeChoice;
     @FXML
     private Label informationText;
 
@@ -49,15 +45,18 @@ public class RegisterStage {
         currentStage.setScene(newScene);
     }
 
-    public void SignUp(ActionEvent event) throws SQLException {
-        String username = tf_username.getText();
-        String password = tf_password.getText();
-        String cfPassword = tf_cfpassword.getText();
-
-        if (checkValidInput(username, password, cfPassword)) {
+    public void SignUpButtonClicked(ActionEvent event) throws SQLException {
+        if (checkValidInput()) {
             try {
+
+                String username = UserNameTF.getText();
+                String password = PasswordTF.getText();
+                String email = EmailTF.getText();
+                Integer age = AgeChoice.getValue();
+                String address = AddressTF.getText();
+
                 UserDataBase userDataBase = new UserDataBase();
-                userDataBase.addNewUser(username, password);
+                userDataBase.addNewUser(username, password, email, address, age);
 
                 showAlert(Alert.AlertType.INFORMATION, "Hệ thống",
                           "Đăng kí thành công");
@@ -83,9 +82,25 @@ public class RegisterStage {
         alert.showAndWait();
     }
 
-    public boolean checkValidInput(String username, String password, String cfPassword) {
+    public boolean checkValidInput() {
+        String username = UserNameTF.getText();
+        String email = EmailTF.getText();
+        Integer age = AgeChoice.getValue();
+        String address = AddressTF.getText();
+        String password = PasswordTF.getText();
+        String cfPassword = CFPasswordTF.getText();
+
         if (username.isEmpty()) {
-            informationText.setText("Vui lòng nhập tên nguòi dùng");
+            informationText.setText("Vui lòng nhập tên người dùng");
+            return false;
+        } else if (email.isEmpty() || !isValidEmail(email)) {
+            informationText.setText("Vui lòng nhập email hợp lệ");
+            return false;
+        } else if (age == null) {
+            informationText.setText("Vui lòng chọn độ tuổi");
+            return false;
+        } else if (address.isEmpty()) {
+            informationText.setText("Vui lòng nhập địa chỉ");
             return false;
         } else if (password.isEmpty()) {
             informationText.setText("Vui lòng nhập mật khẩu");
@@ -98,5 +113,19 @@ public class RegisterStage {
             return false;
         }
         return true;
+    }
+
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        return email.matches(emailRegex);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        for (int i = 5; i < 100; i++) {
+            AgeChoice.getItems().add(i);
+        }
+        AgeChoice.setVisibleRowCount(5);
+        AgeChoice.setValue(18);
     }
 }
